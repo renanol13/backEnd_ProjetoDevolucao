60c2ee5eb95052804c116d2d7c895268e60a88b5
Renan <renan13.com@gmailcom>
2026-02-04 14:39:41 -0300

    Reformulando projeto para backEnd online 3

--- Server.js
+++ Server.js
@@ -1,7 +1,7 @@
 const app = require("./src/App");
-const connection = require("./src/data/Connection")
+const connection = require("./src/data/connection_db.js")
 
-const PORT = process.env.PORT | 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log("Servidor ligado");
